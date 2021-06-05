8af83c863760cdf89e6d6f28d55ca4034b2b6f77
asus <kkw-11@naver.com>
2021-06-05 09:13:16 +0900

    Java Array

---
+++ JAVA/JAVA_workspace/javaStudy/src/javastudy/ArrayExam.java
@@ -0,0 +1,12 @@
+package javastudy;
+
+public class ArrayExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array1 = new int[100];
+		array1[0] = 10;
+		array1[1] = 20;
+	}
+
+}
---
+++ JAVA/JAVA_workspace/javaStudy/src/javastudy/whileExam.java
@@ -0,0 +1,25 @@
+package javastudy;
+
+public class whileExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int i = 0;
+//		
+//		while(i<10) {
+//			System.out.println(i++);
+//		}
+		
+		int total = 0;
+		int i = 1;
+		while(i<=100) {
+			total += i;
+			++i;
+		}
+		System.out.println(total);
+		while(true) {
+			System.out.println("Hello");
+		}
+	}
+
+}
