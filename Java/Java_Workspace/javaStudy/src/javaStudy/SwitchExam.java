702d751f5ccb4314c0a2aa3bd15bb4e06b7cbcc5
asus <kkw-11@naver.com>
2021-06-05 17:13:57 +0900

    directory rename

---
+++ Java/Java_Workspace/javaStudy/src/javaStudy/SwitchExam.java
@@ -0,0 +1,39 @@
+package javaStudy;
+
+public class SwitchExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//switch, case, default, break
+		
+		int value = 1;
+		
+		switch(value) {
+			case 1:
+				System.out.println("1");
+				break;
+			case 2:
+				System.out.println("2");
+				break;
+			case 3:
+				System.out.println("3");
+				break;
+			default:
+				System.out.println("others");
+		
+		}
+		
+		String str = "A";
+		switch(str) {
+		case "A":
+			System.out.println("A");
+			break;
+		case "B":
+			System.out.println("B");
+			
+		}
+				
+		
+	}
+
+}
