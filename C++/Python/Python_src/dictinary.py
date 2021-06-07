e2bc9baa9299b9a0087819058296d33cbeb75994
asus <kkw-11@naver.com>
2021-06-07 16:00:47 +0900

    dictinary.md first commit

---
+++ C++/Python/Python_src/dictinary.py
@@ -0,0 +1,12 @@
+x = {'a':1,'b':2,'c':3}
+
+#print("key->",end = " ")
+#for i in x:
+#    print(i, end = ' ')
+
+#print()
+#for key, value in x.items():
+#    print(key, value)
+
+for i in x:
+    print(i,x[i])
