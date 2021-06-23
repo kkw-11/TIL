f9029569e471cd1f397305ed73892c402dccc15e
asus <kkw-11@naver.com>
2021-06-23 15:15:28 +0900

    git test

---
+++ Python/Python_src/hello_world.py
@@ -0,0 +1,10 @@
+def complicate_function(a):
+        a+=1000
+        a*= 0.5
+        '''
+        very complicated
+        '''
+        a = "string"
+
+if __name__ == "__main__":
+    print("hello world")
