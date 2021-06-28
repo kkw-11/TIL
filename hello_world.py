48c44b5401aa94e017caa17860b1650e60b23a5b
asus <kkw-11@naver.com>
2021-06-28 11:34:00 +0900

    elice

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
-print("hello world1")
+words = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+for i in range(len(words)) :
+    wd = "hit"
+    print([j for j in range(len(words[i])) if words[i][j] != wd[j]])
