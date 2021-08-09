c1b6c0bc7fcb49ab42c528993ddcb2f04c07843b
asus <kkw-11@naver.com>
2021-08-09 16:07:33 +0900

    210809 elice class

---
+++ Elice_AI_Track/Class/8week/210809/sideproject_covid19/CovidMap (5).js
@@ -0,0 +1,198 @@
+import React, { useEffect, useState } from "react";
+import {
+  Seoul,
+  Gyeonggi,
+  Incheon,
+  Gangwon,
+  Chungnam,
+  Chungbuk,
+  Sejong,
+  Daejeon,
+  Gyeongnam,
+  Gyeongbuk,
+  Jeonbuk,
+  Jeonnam,
+  Ulsan,
+  Busan,
+  Daegu,
+  Gwangju,
+  Jeju,
+} from "./area/all_area";
+
+import { BrowserRouter as Router, Switch,Route,Link } from "react-router-dom";
+
+import axios from "axios";
+
+const fillColor = ["#4088da", "#ffb911", "#fc7001", "#e60000"];
+
+function CovidInfo({area,num,level,updated_data}) {
+  return (
+    <div>
+      <h1>대한민국 코로나 현황</h1>
+      <h2>{area}</h2>
+      <p>확진자 수 : {num}</p>
+      <p>거리두기 단계 : {level}</p>
+      <p>{updated_data} (기준)</p>
+    </div>
+  );
+}
+
+function CovidSvg({covidData , onClick}) {
+  return (
+    <svg width="700px" height="1000px" viewBox="0 0 800 1200">
+        <Seoul
+          fill={fillColor[covidData["서울"].level - 1]}
+          onClick={onClick}
+        />
+        <Gyeonggi
+          fill={fillColor[covidData["경기"].level - 1]}
+          onClick={onClick}
+        />
+        <Gangwon
+          fill={fillColor[covidData["강원"].level - 1]}
+          onClick={onClick}
+        />
+        <Incheon
+          fill={fillColor[covidData["인천"].level - 1]}
+          onClick={onClick}
+        />
+        <Chungnam
+          fill={fillColor[covidData["충남"].level - 1]}
+          onClick={onClick}
+        />
+        <Chungbuk
+          fill={fillColor[covidData["충북"].level - 1]}
+          onClick={onClick}
+        />
+        <Sejong
+          fill={fillColor[covidData["세종"].level - 1]}
+          onClick={onClick}
+        />
+        <Daejeon
+          fill={fillColor[covidData["대전"].level - 1]}
+          onClick={onClick}
+        />
+        <Gyeongnam
+          fill={fillColor[covidData["경남"].level - 1]}
+          onClick={onClick}
+        />
+        <Gyeongbuk
+          fill={fillColor[covidData["경북"].level - 1]}
+          onClick={onClick}
+        />
+        <Jeonbuk
+          fill={fillColor[covidData["전북"].level - 1]}
+          onClick={onClick}
+        />
+        <Jeonnam
+          fill={fillColor[covidData["전남"].level - 1]}
+          onClick={onClick}
+        />
+        <Ulsan
+          fill={fillColor[covidData["울산"].level - 1]}
+          onClick={onClick}
+        />
+        <Busan
+          fill={fillColor[covidData["부산"].level - 1]}
+          onClick={onClick}
+        />
+        <Daegu
+          fill={fillColor[covidData["대구"].level - 1]}
+          onClick={onClick}
+        />
+        <Gwangju
+          fill={fillColor[covidData["광주"].level - 1]}
+          onClick={onClick}
+        />
+        <Jeju
+          fill={fillColor[covidData["제주"].level - 1]}
+          onClick={onClick}
+        />
+      </svg>
+  )
+}
+
+
+function CovidMap() {
+  const [covidData, setCovidData] = useState(null);
+  const [selectData, setSelectData] = useState(null);
+
+  useEffect(() => {
+    console.log("업데이트 된 항목", covidData);
+  }, [covidData]);
+
+  function fetch() {
+    axios.post("http://localhost:5000/covidData").then((response) => {
+      // 요청 성공 시 실행되는 코드
+      setCovidData(response.data);
+    });
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      fetch();
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  function handleClick(e) {
+    const area = e.target.id;
+    setSelectData({
+      area,
+      num: covidData.data[area].num,
+      level: covidData.data[area].level,
+    });
+  }
+
+  return covidData === null ? (
+    <div>Loading</div>
+  ) : (
+    <div>
+      {selectData && <CovidInfo area={selectData.area} num={selectData.num} level={selectData.level} updated_data={covidData.updated_data} />}
+      <CovidSvg covidData={covidData.data} onClick={handleClick} />
+    </div>
+  );
+}
+
+function Test() {
+  return (
+    <div>
+      Hello React
+    </div>
+  )
+}
+
+function Select() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">CovidMap</Link>
+          </li>
+          <li>
+            <Link to="/test">Test</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+            <CovidMap />
+        </Route>
+        <Route path="/test">
+            <Test />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default Select;
---
+++ Elice_AI_Track/Class/8week/210809/sideproject_covid19/app.py
@@ -0,0 +1,12 @@
+import './App.css';
+import Select from './CovidMap';
+
+function App() {
+  return (
+    <div className="App">
+        <Select />
+    </div>
+  );
+}
+
+export default App;
